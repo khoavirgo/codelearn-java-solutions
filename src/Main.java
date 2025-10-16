7cbc4a39ca6c08b18aede1e98dc15c82a86a25c2
khoavirgo <ndangkhoa567@gmail.com>
2025-10-17 00:52:55 +0700

    clear main branch

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,4 @@
-import OOP.Account;
-
 public class Main {
     public static void main(String[] args) {
-        Account account1 = new Account(1000, "Tuan", 2000);
-        account1.display();
-        account1.withdraw(2100);
-        account1.deposit(600);
-        System.out.println("Balance: " + account1.getBalance());
-        account1.withdraw(2100);
-        System.out.println("Balance: " + account1.getBalance());
     }
 }
